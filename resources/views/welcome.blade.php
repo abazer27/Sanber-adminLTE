c9e6ffaa16acdebe2f2b0807fe8a7dd952feac45
Akbar Hazura <akbarhazura@gmail.com>
2021-04-08 05:55:19 +0700

    input tugas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/master');
+
+@section('title')
+    SanberCode
+@endsection
+
+@section('title2')
+    Laravel Day 3
+@endsection
+
+@section('content')
+    Tugas template laravel Akbar Hazura Neldi
+@endsection
